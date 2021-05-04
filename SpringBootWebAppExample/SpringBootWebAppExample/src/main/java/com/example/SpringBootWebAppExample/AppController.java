99ada2a9edde80f9a8abb1e6f75fa3a0be719e96
NehaSwami <40225371+NehaSwami@users.noreply.github.com>
2021-05-05 00:48:18 +0530

    Add files via upload

---
+++ SpringBootWebAppExample/SpringBootWebAppExample/src/main/java/com/example/SpringBootWebAppExample/AppController.java
@@ -0,0 +1,22 @@
+package com.example.SpringBootWebAppExample;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+ 
+@Controller
+public class AppController {
+     
+    @RequestMapping("/list_contact")
+    public String listContact(Model model) {
+         
+        ContactBusiness business = new ContactBusiness();
+        List<Contact> listContact = business.getContactList();
+         
+        model.addAttribute("contacts", listContact);       
+         
+        return "contact";
+    }
+}
